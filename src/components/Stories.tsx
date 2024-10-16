6112969c12b2e53ec2da0e0990026569d256cf96
Xavier <xavierh93@hotmail.com>
2024-10-16 19:11:47 -0400

    implement stories component

---
+++ src/components/Stories.tsx
@@ -0,0 +1,122 @@
+import Image from 'next/image';
+
+const Stories = () => {
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs scrollbar-hide">
+      <div className="flex gap-8 w-max">
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/1323550/pexels-photo-1323550.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+            height={80}
+            width={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-small">Elon</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/1323550/pexels-photo-1323550.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+            height={80}
+            width={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-small">Elon</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/1323550/pexels-photo-1323550.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+            height={80}
+            width={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-small">Elon</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/1323550/pexels-photo-1323550.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+            height={80}
+            width={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-small">Elon</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/1323550/pexels-photo-1323550.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+            height={80}
+            width={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-small">Elon</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/1323550/pexels-photo-1323550.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+            height={80}
+            width={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-small">Elon</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/1323550/pexels-photo-1323550.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+            height={80}
+            width={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-small">Elon</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/1323550/pexels-photo-1323550.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+            height={80}
+            width={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-small">Elon</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/1323550/pexels-photo-1323550.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+            height={80}
+            width={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-small">Elon</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/1323550/pexels-photo-1323550.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+            height={80}
+            width={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-small">Elon</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Stories;
