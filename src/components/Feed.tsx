29b8886a3e3ff93494cab419b20d0ab848831985
Xavier <xavierh93@hotmail.com>
2024-10-16 19:45:31 -0400

    feed component

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,9 +1,13 @@
+import Post from './Post';
+
 const Feed = () => {
   return (
-    <div className="flex gap-6">
-      <div className="hidden xl:block w-[20%]"></div>
-      <div className="hidden lg:w-[70%] xl:w-[50%]"></div>
-      <div className="hidden lg:block w-[30%]"></div>
+    <div className="flex p-4 bg-white shadow-md flex-col gap-12">
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
     </div>
   );
 };
