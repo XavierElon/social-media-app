b47271a163667c012ba47cd5871557cb740b987e
Xavier <xavierh93@hotmail.com>
2024-10-16 19:11:39 -0400

    leftmenu

---
+++ src/components/LeftMenu.tsx
@@ -0,0 +1,11 @@
+const LeftMenu = () => {
+    return (
+      <div className='flex gap-6'>
+        <div className="hidden xl:block w-[20%]"></div>
+        <div className="hidden lg:w-[70%] xl:w-[50%]"></div>
+        <div className="hidden lg:block w-[30%]"></div>
+      </div>
+    )
+  }
+  
+  export default LeftMenu
