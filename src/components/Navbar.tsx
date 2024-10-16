e2289ca3634b62fd214e5bebea1602ac84453143
Xavier <xavierh93@hotmail.com>
2024-10-16 17:55:51 -0400

    mobile menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,51 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import MobileMenu from './MobileMenu';
 
 const Navbar = () => {
   return (
     <div className="h-24 flex items-center justify-between">
       {/* LEFT */}
-      <div className="">
+      <div className="md: hidden lg:block w-[20%]">
         <Link href="/" className="font-bold text-xl text-blue-600">
           X Social
         </Link>
       </div>
       {/* CENTER */}
-      <div className="hidden"></div>
+      <div className="hidden md:flex w-[50%] text-sm,">
+        {/* LINKS */}
+        <div className="flex gap-6 text-gray-600">
+          <Link href="/" className="flex gap-2">
+            <Image
+              src="/home.png"
+              alt="Homepage"
+              width={16}
+              height={16}
+              className="w-4 h-4"
+            />
+          </Link>
+          <Link href="/" className="flex gap-2">
+            <Image
+              src="/friends.png"
+              alt="Friends"
+              width={16}
+              height={16}
+              className="w-4 h-4"
+            />
+          </Link>
+          <Link href="/" className="flex gap-2">
+            <Image
+              src="/stories.png"
+              alt="Stories"
+              width={16}
+              height={16}
+              className="w-4 h-4"
+            />
+          </Link>
+        </div>
+      </div>
       {/* RIGHT */}
-      <div className="">
+      <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
         <MobileMenu />
       </div>
     </div>
