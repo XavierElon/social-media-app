4f355b0cddae8118183915894973ca156a641840
Xavier <xavierh93@hotmail.com>
2024-10-16 19:23:52 -0400

    work on addPost component

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,9 +1,54 @@
+import Image from 'next/image';
+
 const AddPost = () => {
   return (
-    <div className="flex gap-6">
-      <div className="hidden xl:block w-[20%]"></div>
-      <div className="hidden lg:w-[70%] xl:w-[50%]"></div>
-      <div className="hidden lg:block w-[30%]"></div>
+    <div className="flex gap-4 p-4 bg-white shadow-md rounded-lg justify-between">
+      {/* AVATAR */}
+      <Image
+        src="https://images.pexels.com/photos/1323550/pexels-photo-1323550.jpeg?auto=compress&cs=tinysrgb&w=800"
+        alt=""
+        width={48}
+        height={48}
+        className="w-12 h-12 object-cover rounded-full"
+      />
+      {/* POST */}
+      <div className=" flex-1">
+        {/* TEXT INPUT */}
+        <div className="flex gap-4">
+          <textarea
+            placeholder="What is on your mind?"
+            className="flex-1 bg-slate-100 rounded-lg p-2"
+            name=""
+            id=""
+          ></textarea>
+          <Image
+            src="/emoji.png"
+            alt=""
+            width={20}
+            height={20}
+            className="w-5 h-5 cursor-pointer self-end"
+          />
+        </div>
+        {/* POST OPTIONS */}
+        <div className="flex items-center gap-4 mt-4 text-gray-400 flex-wrap">
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/addimage.png" alt="" width={20} height={20} />
+            Photo
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/addvideo.png" alt="" width={20} height={20} />
+            Video
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/addevent.png" alt="" width={20} height={20} />
+            Event
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/poll.png" alt="" width={20} height={20} />
+            Poll
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
