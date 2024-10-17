5ce9c217c0ddc694671fe8f9af67e47ded96f428
Xavier <xavierh93@hotmail.com>
2024-10-17 16:08:07 -0400

    post component

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -33,10 +33,48 @@ const Post = () => {
           mollit est aute eu. Lorem veniam in culpa cupidatat.
         </p>
       </div>
-      {/* INTERACTION */}
+      {/* INTERACTIONS */}
       <div className="flex items-center justify-between text-sm">
-        <div className="">
-          <div className="flex items-center gap-4 bg-slate-100 p2 rounded-xl"></div>
+        <div className="flex gap-8">
+          <div className="flex items-center gap-4 bg-slate-100 p2 rounded-xl">
+            <Image
+              src="/like.png"
+              width={16}
+              height={16}
+              alt=""
+              className="cursor-pointer"
+            />
+            <span className="text-gray-300">|</span>
+            <span className="text-gray-500">
+              123 <span className="hidden md:inline">Likes</span>
+            </span>
+          </div>
+          <div className="flex items-center gap-4 bg-slate-100 p2 rounded-xl">
+            <Image
+              src="/comment.png"
+              width={16}
+              height={16}
+              alt=""
+              className="cursor-pointer"
+            />
+            <span className="text-gray-300">|</span>
+            <span className="text-gray-500">
+              123 <span className="hidden md:inline">Comments</span>
+            </span>
+          </div>
+          <div className="flex items-center gap-4 bg-slate-100 p2 rounded-xl">
+            <Image
+              src="/share.png"
+              width={16}
+              height={16}
+              alt=""
+              className="cursor-pointer"
+            />
+            <span className="text-gray-300">|</span>
+            <span className="text-gray-500">
+              123 <span className="hidden md:inline">Shares</span>
+            </span>
+          </div>
         </div>
         <div className=""></div>
       </div>
